a84b4200a38b32677d99ca4343f760be1d4960ab
Tony Bui <Tony.bui.1@ens.etsmtl.ca>
2026-09-22 17:29:19 -0400

    Configure PostgreSQL and sprintboot datasource.

---
+++ src/main/java/payment_infra/PaymentInfraApplication.java
@@ -0,0 +1,13 @@
+package payment_infra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentInfraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentInfraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/payment_infra/PaymentInfraApplicationTests.java
@@ -0,0 +1,13 @@
+package payment_infra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentInfraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
